11c98ef92cf857fdaa76bc0306e73959222f46a8
Alizée GUEYE <AG@def-systemes.local>
2024-08-12 16:32:13 +0200

    modif avis

--- scripts.js
+++ scripts.js
@@ -1,70 +1,103 @@
 document.addEventListener('DOMContentLoaded', function() {
     const avisForm = document.getElementById('reviewForm');
     const temoignagesList = document.getElementById('temoignages-list');
+    const previewContainer = document.getElementById('previewContainer');
+    const previewList = document.getElementById('previewList');
+    const confirmButton = document.getElementById('confirmButton');
+    const prevButton = document.getElementById('prevButton');
+    const nextButton = document.getElementById('nextButton');
+    const clientId = '2f6cc0604610439'; // Remplacez par votre vrai Client ID Imgur
+    const itemsPerPage = 4; // Nombre d'avis visibles par page
+    let currentPage = 0;
 
-    if (avisForm && temoignagesList) {
-        avisForm.addEventListener('submit', async function(event) {
-            event.preventDefault();
+    avisForm.addEventListener('submit', async function(event) {
+        event.preventDefault();
 
-            const nom = document.getElementById('name').value;
-            const prenom = document.getElementById('prenom').value;
-            const avis = document.getElementById('message').value;
-            const file = document.getElementById('file').files[0];
-            const clientId = '2f6cc0604610439'; // Remplacez par votre vrai Client ID Imgur
+        const nom = document.getElementById('name').value;
+        const prenom = document.getElementById('prenom').value;
+        const avis = document.getElementById('message').value;
+        const file = document.getElementById('file').files[0];
+        let imageUrl = '';
 
-            if (nom && prenom && avis) {
-                let imageUrl = '';
+        if (nom && prenom && avis) {
+            if (file) {
+                const formData = new FormData();
+                formData.append('image', file);
 
-                if (file) {
-                    const formData = new FormData();
-                    formData.append('image', file);
+                try {
+                    const response = await fetch('https://api.imgur.com/3/image', {
+                        method: 'POST',
+                        headers: {
+                            Authorization: `Client-ID ${clientId}`,
+                        },
+                        body: formData,
+                    });
 
-                    try {
-                        const response = await fetch('https://api.imgur.com/3/image', {
-                            method: 'POST',
-                            headers: {
-                                Authorization: `Client-ID ${clientId}`,
-                            },
-                            body: formData,
-                        });
-
-                        const result = await response.json();
-
-                        if (result.success) {
-                            imageUrl = result.data.link;
-                        } else {
-                            console.error('Erreur lors du téléchargement sur Imgur:', result.data.error);
-                        }
-                    } catch (error) {
-                        console.error('Erreur lors du téléchargement:', error);
+                    if (!response.ok) {
+                        throw new Error(`HTTP error! Status: ${response.status}`);
                     }
+
+                    const result = await response.json();
+                    if (result.success && result.data) {
+                        imageUrl = result.data.link;
+                    } else {
+                        console.error('Erreur lors du téléchargement sur Imgur:', result.data ? result.data.error : 'Erreur inconnue');
+                    }
+                } catch (error) {
+                    console.error('Erreur lors du téléchargement:', error);
                 }
-
-                const newAvis = document.createElement('div');
-                newAvis.classList.add('temoignage');
-                newAvis.innerHTML = `
-                    <div class="photo-container">
-                        <img src="${imageUrl}" alt="Photo" class="temoignage-photo">
-                    </div>
-                    <div class="temoignage-message">
-                        <span class="quote-icon">“</span>
-                        <p class="message-texte">${avis}</p>
-                        <span class="quote-icon2">”</span>
-                    </div>
-                    <div class="temoignage-nom">
-                        <p>${nom} ${prenom}</p>
-                    </div>
-                `;
-
-                temoignagesList.prepend(newAvis);
-                avisForm.reset();
-            } else {
-                console.error("Tous les champs doivent être remplis.");
             }
+
+            const previewItem = document.createElement('div');
+            previewItem.classList.add('preview-item');
+            previewItem.innerHTML = `
+                <div class="photo-container">
+                    <img src="${imageUrl}" alt="Photo" class="temoignage-photo">
+                </div>
+                <div class="temoignage-message">
+                    <p class="message-texte">${avis}</p>
+                </div>
+                <div class="temoignage-nom">
+                    <p>${nom} ${prenom}</p>
+                </div>
+            `;
+            previewList.appendChild(previewItem);
+        }
+    });
+
+    confirmButton.addEventListener('click', function() {
+        previewContainer.style.display = 'none'; // Cacher la prévisualisation
+        const items = previewList.querySelectorAll('.preview-item');
+
+        items.forEach(item => {
+            const newAvis = document.createElement('div');
+            newAvis.classList.add('temoignage');
+            newAvis.innerHTML = item.innerHTML;
+            temoignagesList.appendChild(newAvis);
         });
-    } else {
-        console.error('Le formulaire ou la liste des témoignages est introuvable.');
+
+        previewList.innerHTML = ''; // Réinitialiser la prévisualisation
+    });
+
+    function updateCarousel() {
+        const offset = -currentPage * (100 / itemsPerPage);
+        temoignagesList.style.transform = `translateX(${offset}%)`;
     }
+
+    prevButton.addEventListener('click', function() {
+        if (currentPage > 0) {
+            currentPage--;
+            updateCarousel();
+        }
+    });
+
+    nextButton.addEventListener('click', function() {
+        const totalPages = Math.ceil(temoignagesList.children.length / itemsPerPage) - 1;
+        if (currentPage < totalPages) {
+            currentPage++;
+            updateCarousel();
+        }
+    });
 
     // Gestion des autres fonctionnalités
     document.querySelectorAll('a[href^="#"]').forEach(anchor => {
@@ -125,9 +158,9 @@ document.addEventListener('DOMContentLoaded', function() {
     showNextMessage(); // Show the first message immediately
 
     function toggleMusic() {
-        var audio = document.getElementById('background-music');
-        var button = document.getElementById('music-toggle');
-        var icon = document.getElementById('music-icon');
+        const audio = document.getElementById('background-music');
+        const button = document.getElementById('music-toggle');
+        const icon = document.getElementById('music-icon');
         if (audio && button && icon) {
             if (audio.paused) {
                 audio.play();
@@ -143,7 +176,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // Fonction pour démarrer la musique à la première interaction utilisateur
     function startMusicOnInteraction() {
-        var audio = document.getElementById('background-music');
+        const audio = document.getElementById('background-music');
         if (audio) {
             audio.play();
             document.removeEventListener('click', startMusicOnInteraction);
