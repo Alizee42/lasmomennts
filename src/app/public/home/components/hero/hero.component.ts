b046b429cbaecb828f15258c8d484c6e1b018faf
Alizee42 <119494232+Alizee42@users.noreply.github.com>
2026-06-19 15:51:59 +0200

    feat(hero): bouton musique de fond afrobeat - Audio en boucle à 30% de volume, muet par défaut - Bouton play/pause rond en bas à droite du hero - Icône volume-up/volume-mute selon l'état

--- src/app/public/home/components/hero/hero.component.ts
+++ src/app/public/home/components/hero/hero.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ApiUrlPipe } from '../../../../core/pipes/api-url.pipe';
 import { SiteConfig } from '../../../../core/models/site-config.model';
@@ -10,6 +10,24 @@ import { SiteConfig } from '../../../../core/models/site-config.model';
   templateUrl: './hero.component.html',
   styleUrls: ['./hero.component.scss']
 })
-export class HeroComponent {
+export class HeroComponent implements AfterViewInit {
   @Input() config!: SiteConfig;
+  @ViewChild('bgMusic') bgMusic!: ElementRef<HTMLAudioElement>;
+
+  playing = false;
+
+  ngAfterViewInit() {
+    const audio = this.bgMusic.nativeElement;
+    audio.volume = 0.3;
+  }
+
+  toggleMusic() {
+    const audio = this.bgMusic.nativeElement;
+    if (this.playing) {
+      audio.pause();
+    } else {
+      audio.play();
+    }
+    this.playing = !this.playing;
+  }
 }
